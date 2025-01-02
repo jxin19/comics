9e735ff2fbe960f23acd2af02a14480832e0a319
신민준 <jxin95@gmail.com>
2025-01-02 16:32:09 +0900

    fix: 구매내역 더미데이터 생산쿼리 오류 수정

--- src/main/resources/db/migration/V10__insert_purchase_data.sql
+++ src/main/resources/db/migration/V10__insert_purchase_data.sql
@@ -1,28 +1,44 @@
-
-DO
-$$
+DO $$
 DECLARE
-batch_size INT := 25;
-    total_size
-INT := 100;
-    batch_count
-INT := total_size / batch_size;
-    i
-INT;
+batch_size INT := 5000;
+    total_size INT := 100000;
+    batch_count INT := total_size / batch_size;
+    dummy_start_date TIMESTAMP WITH TIME ZONE := '2024-12-01 00:00:00+00';
+    dummy_end_date TIMESTAMP WITH TIME ZONE := '2024-12-30 23:59:59+00';
+    i INT;
 BEGIN
 FOR i IN 1..batch_count LOOP
-        INSERT INTO purchase (member_id, work_id, paid_price, purchased_at)
-SELECT FLOOR(RANDOM() * (SELECT COUNT(*) FROM member) + 1)::INT AS member_id, wpp.work_id AS work_id,
-       wpp.price AS paid_price,
-       TIMESTAMP '2024-12-01 00:00:00' +
-       (RANDOM() * (EXTRACT(EPOCH FROM TIMESTAMP '2024-12-30 23:59:59' -
-                                       TIMESTAMP '2024-12-01 00:00:00')) || ' seconds') ::INTERVAL AS purchased_at
-FROM work_pricing_policy wpp
-         JOIN generate_series(1, batch_size) g ON TRUE
-WHERE TIMESTAMP '2024-12-01 00:00:00' +
-      (RANDOM() * (EXTRACT(EPOCH FROM TIMESTAMP '2024-12-30 23:59:59' -
-                                      TIMESTAMP '2024-12-01 00:00:00')) || ' seconds')::INTERVAL
-            BETWEEN wpp.start_date AND COALESCE(wpp.end_date, 'infinity')
-ON CONFLICT (member_id, work_id) DO NOTHING;
+        WITH random_member_work AS (
+            SELECT
+                m.member_id,
+                wpp.work_id,
+                wpp.price as paid_price
+            FROM member m
+            CROSS JOIN LATERAL (
+                SELECT w.work_id, wpp.price
+                FROM work w
+                JOIN work_pricing_policy wpp ON w.work_id = wpp.work_id
+                WHERE NOT wpp.is_free
+                AND wpp.start_date <= now()
+                AND (wpp.end_date IS NULL OR wpp.end_date >= now())
+                ORDER BY RANDOM()
+                LIMIT 1
+            ) wpp
+            ORDER BY RANDOM()
+            LIMIT batch_size
+        )
+        INSERT INTO purchase (
+            member_id,
+            work_id,
+            paid_price,
+            purchased_at
+        )
+SELECT
+    member_id,
+    work_id,
+    paid_price,
+    dummy_start_date + (random() * (dummy_end_date - dummy_start_date))
+FROM random_member_work
+    ON CONFLICT (member_id, work_id) DO NOTHING;
 END LOOP;
 END $$;
