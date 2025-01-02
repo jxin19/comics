749d76b6b0097a4215a87b93044ef9057b1f038d
신민준 <jxin95@gmail.com>
2025-01-02 15:21:10 +0900

    test: 테스트 애플리케이션 클래스 추가

---
+++ src/test/kotlin/com/homework/comics/ComicsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.homework.comics
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ComicsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
